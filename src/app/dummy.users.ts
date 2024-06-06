cbfa057c875ade2568bb3d959a801cee2830fdea
MUFFANUJ <anujsinghhero292@gmail.com>
2024-06-06 10:11:47 +0530

    new angular project

---
+++ src/app/dummy.users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Jasmine Washington',
+      avatar: 'https://img.freepik.com/free-photo/lifestyle-beauty-fashion-people-emotions-concept-young-asian-female-office-manager-ceo-with-pleased-expression-standing-white-background-smiling-with-arms-crossed-chest_1258-59329.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Emily Thompson',
+      avatar: 'https://st.depositphotos.com/1158045/4659/i/450/depositphotos_46591045-stock-photo-man-holding-a-portrait-of.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Marcus Johnson',
+      avatar: 'https://images.pexels.com/photos/1181717/pexels-photo-1181717.jpeg?cs=srgb&dl=pexels-divinetechygirl-1181717.jpg&fm=jpg',
+    },
+    {
+      id: 'u4',
+      name: 'David Miller',
+      avatar: 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=sph',
+    },
+    {
+      id: 'u5',
+      name: 'Priya Patel',
+      avatar: 'https://t4.ftcdn.net/jpg/02/61/52/95/360_F_261529596_YZWJaMnYFSCM0FSCrxs71o6RrZ9MpP4D.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Arjun Singh',
+      avatar: 'user-6.jpg',
+    },
+  ];
